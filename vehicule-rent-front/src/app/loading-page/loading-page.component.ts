25625f3533ac7ba7d6ae11b448452bb86df845ab
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-03-27 22:16:12 +0100

    Added redirection and fixed login

--- vehicule-rent-front/src/app/loading-page/loading-page.component.ts
+++ vehicule-rent-front/src/app/loading-page/loading-page.component.ts
@@ -1,23 +1,35 @@
-import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
-import {SessionService} from "../shared/session-service";
-
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Router } from '@angular/router';
+import { KeycloakService } from '../shared/keycloak.service';
 
 @Component({
-changeDetection: ChangeDetectionStrategy.OnPush,
-    selector: 'app-loading-page',
-    imports: [],
-    templateUrl: './loading-page.component.html',
-    styleUrl: './loading-page.component.css'
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-loading-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading-page.component.html',
+  styleUrl: './loading-page.component.css'
 })
-export class LoadingPageComponent implements OnInit{
+export class LoadingPageComponent implements OnInit {
 
-  constructor(private sessionService: SessionService) {}
-
+  constructor(
+    private keycloakService: KeycloakService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    const roles = this.keycloakService.getRoles();
 
+    if (roles.includes('admin')) {
+      this.router.navigate(['/admin']);
+    } else if (roles.includes('client')) {
+      this.router.navigate(['/client']);
+    } else if (roles.includes('supplier')) {
+      this.router.navigate(['/supplier']);
+    } else if (roles.includes('repair')) {
+      this.router.navigate(['/repair']);
+    } else {
+      this.router.navigate(['/select-role']);
+    }
   }
-
-
-
 }
