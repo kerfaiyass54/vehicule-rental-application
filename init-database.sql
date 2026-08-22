517ce6f32662f7731afd5dc9c146fc997c85aa2d
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-08-22 13:35:15 +0100

    Added volumes for the data with insertion instructions

---
+++ init-database.sql
@@ -0,0 +1,18 @@
+\i /docker-entrypoint-initdb.d/schema/init.sql
+
+\i /docker-entrypoint-initdb.d/data/admin.sql
+\i /docker-entrypoint-initdb.d/data/location.sql
+\i /docker-entrypoint-initdb.d/data/supplier.sql
+\i /docker-entrypoint-initdb.d/data/address.sql
+\i /docker-entrypoint-initdb.d/data/client.sql
+\i /docker-entrypoint-initdb.d/data/repair.sql
+\i /docker-entrypoint-initdb.d/data/vehicle.sql
+\i /docker-entrypoint-initdb.d/data/buying.sql
+\i /docker-entrypoint-initdb.d/data/ticket.sql
+\i /docker-entrypoint-initdb.d/data/demand.sql
+\i /docker-entrypoint-initdb.d/data/repairInfo.sql
+\i /docker-entrypoint-initdb.d/data/subscription.sql
+
+\i /docker-entrypoint-initdb.d/schema/seq.sql
+
+\i /docker-entrypoint-initdb.d/schema/indexes.sql
